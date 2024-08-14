5b5a68868a878b695b68691f7c465a3e9ca182fa
Lisa du Plessis <lisadup21@gmail.com>
2024-08-14 22:32:09 +0200

    Added the checkout functions

--- checkout.php
+++ checkout.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'config.php';
 
 session_start();
@@ -6,28 +7,163 @@ session_start();
 $user_id = $_SESSION['user_id'];
 
 if(!isset($user_id)){
-    header('location:login.php');
+   header('location:login.php');
 }
+
+if(isset($_POST['order_btn'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $number = $_POST['number'];
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $method = mysqli_real_escape_string($conn, $_POST['method']);
+   $address = mysqli_real_escape_string($conn, 'flat no. '. $_POST['flat'].', '. $_POST['street'].', '. $_POST['city'].', '. $_POST['country'].' - '. $_POST['pin_code']);
+   $placed_on = date('d-M-Y');
+
+   $cart_total = 0;
+   $cart_products[] = '';
+
+   $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('Query Failed');
+   if(mysqli_num_rows($cart_query) > 0){
+      while($cart_item = mysqli_fetch_assoc($cart_query)){
+         $cart_products[] = $cart_item['name'].' ('.$cart_item['quantity'].') ';
+         $sub_total = ($cart_item['price'] * $cart_item['quantity']);
+         $cart_total += $sub_total;
+      }
+   }
+
+   $total_products = implode(', ',$cart_products);
+
+   $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND address = '$address' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('Query failed');
+
+   if($cart_total == 0){
+      $_SESSION['message'] = 'Your Cart Is Empty';
+   }else{
+      if(mysqli_num_rows($order_query) > 0){
+        $_SESSION['message'] = 'Order Already Placed!'; 
+      }else{
+         mysqli_query($conn, "INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$address', '$total_products', '$cart_total', '$placed_on')") or die('Query failed');
+         $_SESSION['message'] = 'Order Placed Successfully';
+         mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('Query Failed');
+      }
+   }
+   
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checkout</title>
-    <!-- font awesome cdn link  -->
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Checkout</title>
+
+   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <!--CSS file link-->
-    <link rel="stylesheet" href="style.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-    <?php include 'header.php'?>
+   
+<?php include 'header.php'; ?>
 
-    <?php include 'footer.php'?>
+<div class="heading">
+   <h3>Checkout</h3>
+   <p> <a href="home.php">Home</a> / Checkout </p>
+</div>
 
-    <!--custom js link-->
-    <script src="script.js"></script>
-    
+<section class="display-order">
+
+   <?php  
+      $grand_total = 0;
+      $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('Query Failed');
+      if(mysqli_num_rows($select_cart) > 0){
+         while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+            $total_price = ($fetch_cart['price'] * $fetch_cart['quantity']);
+            $grand_total += $total_price;
+   ?>
+   <p> <?php echo $fetch_cart['name']; ?> <span>(<?php echo 'R'.$fetch_cart['price'].'.00'.' x '. $fetch_cart['quantity']; ?>)</span> </p>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">Your Cart Is Empty</p>';
+   }
+   ?>
+   <div class="grand-total"> Grand Total : <span>R<?php echo $grand_total; ?>.00</span> </div>
+
+</section>
+
+<section class="checkout">
+
+   <form action="" method="post">
+      <h3>Place Your Order</h3>
+      <div class="flex">
+         <div class="inputBox">
+            <span>Full Name :</span>
+            <input type="text" name="name" required placeholder="e.g John Doe">
+         </div>
+         <div class="inputBox">
+            <span>Number :</span>
+            <input type="number" name="number" required placeholder="e.g 0892359016">
+         </div>
+         <div class="inputBox">
+            <span>Email :</span>
+            <input type="email" name="email" required placeholder="e.g johndoe@gmail.com">
+         </div>
+         <div class="inputBox">
+            <span>Payment Method :</span>
+            <select name="method">
+               <option value="cash on delivery">Cash On Delivery</option>
+               <option value="credit card">Credit Card</option>
+               <option value="paypal">Paypal</option>
+               <option value="paytm">Paytm</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>Address Line 01 :</span>
+            <input type="number" min="0" name="flat" required placeholder="e.g. Flat No.">
+         </div>
+         <div class="inputBox">
+            <span>Address line 01 :</span>
+            <input type="text" name="street" required placeholder="e.g. Street Name">
+         </div>
+         <div class="inputBox">
+            <span>City :</span>
+            <input type="text" name="city" required placeholder="e.g. Port Elizabeth">
+         </div>
+         <div class="inputBox">
+            <span>Province :</span>
+            <input type="text" name="state" required placeholder="e.g. Eastern Cape">
+         </div>
+         <div class="inputBox">
+            <span>Country :</span>
+            <input type="text" name="country" required placeholder="e.g. South Africa">
+         </div>
+         <div class="inputBox">
+            <span>Pin code :</span>
+            <input type="number" min="0" name="pin_code" required placeholder="e.g. 123456">
+         </div>
+      </div>
+      <input type="submit" value="order now" class="btn" name="order_btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
 </body>
 </html>
