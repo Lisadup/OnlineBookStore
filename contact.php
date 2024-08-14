37af8da93f325bbefe949e1460cd01c7e8461827
Lisa du Plessis <lisadup21@gmail.com>
2024-08-14 19:15:42 +0200

    Created Contact Page

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'config.php';
 
 session_start();
@@ -6,28 +7,77 @@ session_start();
 $user_id = $_SESSION['user_id'];
 
 if(!isset($user_id)){
-    header('location:login.php');
+   header('location:login.php');
 }
+
+if(isset($_POST['send'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $number = $_POST['number'];
+   $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+   $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' AND number = '$number' AND message = '$msg'") or die('query failed');
+
+   if(mysqli_num_rows($select_message) > 0){
+      $_SESSION['message'] = 'Message Sent Already!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `message`(user_id, name, email, number, message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
+      $_SESSION['message'] = 'Message Sent Successfully!';
+   }
+
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-    <!-- font awesome cdn link  -->
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Contact</title>
+
+   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <!--CSS file link-->
-    <link rel="stylesheet" href="style.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    <?php include 'header.php'?>
+   
+<?php include 'header.php'; ?>
 
-    <?php include 'footer.php'?>
+<div class="heading">
+   <h3>Contact Us</h3>
+   <p> <a href="home.php">Home</a> / Contact </p>
+</div>
 
-    <!--custom js link-->
-    <script src="script.js"></script>
-    
+<section class="contact">
+
+   <form action="" method="post">
+      <h3>Say Something</h3>
+      <input type="text" name="name" required placeholder="Full Name:" class="box">
+      <input type="email" name="email" required placeholder="Email:" class="box">
+      <input type="number" name="number" required placeholder="Number" class="box">
+      <textarea name="message" class="box" placeholder="Enter Message" id="" cols="30" rows="10"></textarea>
+      <input type="submit" value="send message" name="send" class="btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="script.js"></script>
+
 </body>
 </html>
