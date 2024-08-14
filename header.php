6af047f6219946129688828d305e3d587e579a9c
Lisa du Plessis <lisadup21@gmail.com>
2024-08-14 18:52:14 +0200

    Fixed messaging issue

--- header.php
+++ header.php
@@ -1,13 +1,12 @@
 <?php
-if(isset($message)){
-   foreach($message as $message){
-      echo '
-      <div class="message">
-         <span>'.$message.'</span>
-         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-      </div>
-      ';
-   }
+if(isset($_SESSION['message'])){
+   echo '
+   <div class="message">
+      <span>'.$_SESSION['message'].'</span>
+      <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+   </div>
+   ';
+   unset($_SESSION['message']); // Clear the message after displaying it
 }
 ?>
 
@@ -42,8 +41,8 @@ if(isset($message)){
             <a href="search_page.php" class="fas fa-search"></a>
             <div id="user-btn" class="fas fa-user"></div>
             <?php
-               $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-               $cart_rows_number = mysqli_num_rows($select_cart_number); 
+            $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+            $cart_rows_number = mysqli_num_rows($select_cart_number);
             ?>
             <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
          </div>
