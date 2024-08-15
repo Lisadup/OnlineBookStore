4afcfc57c54d533e7427d2712e4ffe80487c35c8
Lisa du Plessis <lisadup21@gmail.com>
2024-08-15 11:30:11 +0200

    Made changes to fit the new product format

--- shop.php
+++ shop.php
@@ -13,16 +13,18 @@ if (!isset($user_id)) {
 if (isset($_POST['add_to_cart'])) {
 
     $product_name = $_POST['product_name'];
+    $product_author = $_POST['product_author'];
+    $product_genre = $_POST['product_genre'];
     $product_price = $_POST['product_price'];
     $product_image = $_POST['product_image'];
     $product_quantity = $_POST['product_quantity'];
 
-    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('Query Failed');
 
     if (mysqli_num_rows($check_cart_numbers) > 0) {
         $_SESSION['message'] = 'Already Added To Cart';
     } else {
-        mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+        mysqli_query($conn, "INSERT INTO `cart`(user_id, name, author, genre, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
         $_SESSION['message']= 'Book Added To Cart';
     }
 }
@@ -69,9 +71,13 @@ if (isset($_POST['add_to_cart'])) {
                     <form action="" method="post" class="box">
                         <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
                         <div class="name"><?php echo $fetch_products['name']; ?></div>
+                        <div class="author"><?php echo $fetch_products['author']; ?></div>
+                        <div class="genre"><?php echo $fetch_products['genre']; ?></div>
                         <div class="price">R<?php echo $fetch_products['price']; ?>.00</div>
                         <input type="number" min="1" name="product_quantity" value="1" class="qty">
                         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+                        <input type="hidden" name="product_author" value="<?php echo $fetch_products['author']; ?>">
+                        <input type="hidden" name="product_genre" value="<?php echo $fetch_products['genre']; ?>">
                         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
                         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
                         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
